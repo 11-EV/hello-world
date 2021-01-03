d5b0c814e23d547cf910a98cd22c56b825b8a7b5
11-EV <69338248+11-EV@users.noreply.github.com>
2021-01-03 09:34:11 +0800

    Create README.cpp
    just so so

---
+++ README.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+using namespace std;
+template<class T>
+class WEST:public T{
+public:
+   WEST(T b);
+   show()
+private:
+   T a[90];
+}
+template<class T>
+WEST<T>::WEST(T):a(b){
+      show();
+}
+template<class T>
+WEST<T>::show(){
+    cout<<"find"<<a;
+}
+
+int main(){
+   WEST<int>y(90);
+   return 0;
+}
+
+
+
